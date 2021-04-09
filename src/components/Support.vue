f178d64319ed0d2dc00ef2e3c3209e9149baff93
triandamai <triannurizkillah@gmail.com>
2021-04-09 18:40:16 +0700

    feat(deploy): prepare for deploy

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -1,26 +1,26 @@
 <template>
-	<div id="support">
-		<!-- <h1 class="uk-heading-line uk-text-center uk-h1 uk-padding-small title" uk-scrollspy="cls:uk-animation-slide-top-small"><span>Supported</span></h1> -->
-		<h2>Supported</h2>
-		<div class="wrapper">
-			<!-- <div class="uk-responsive-width uk-visible@l">
-				<ul class="uk-list uk-responsive-width">
+  <div id="support">
+    <!-- <h1 class="uk-heading-line uk-text-center uk-h1 uk-padding-small title" uk-scrollspy="cls:uk-animation-slide-top-small"><span>Supported</span></h1> -->
+    <h2>Supported</h2>
+    <div class="wrapper">
+      <div class="uk-responsive-width uk-visible@l">
+        <!-- <ul class="uk-list uk-responsive-width">
 					<li>1. Hosting By <a href="http://diundang.com" target="_blank">diundang.com</a> - <a href="https://www.facebook.com/drs11" target="_blank">Daeng Rosanda</a></li>
 				    <li>2. Basic Template By <a href="http://codecerdas.com" target="_blank">codecerdas.com</a> - <a href="https://www.facebook.com/chandika.nurdiansyah.9" target="_blank">Chandika Nurdiansyah</a></li>
 				    <li>3. Videoshoot, Photoshoot And Editing By <a href="https://www.facebook.com/fachreza.i.akbar" target="_blank">Fachreza Iryansyah</a></li>
 				 </ul> -->
-			</div>
-			<div class="uk-responsive-width uk-hidden@l">
-				<!-- <ul class="uk-list uk-responsive-width">
+      </div>
+      <div class="uk-responsive-width uk-hidden@l">
+        <!-- <ul class="uk-list uk-responsive-width">
 					<li>1. Hosting By <a href="http://diundang.com" target="_blank">diundang.com</a> - <a href="https://www.facebook.com/drs11" target="_blank">Daeng Rosanda</a></li>
 				    <li>2. Basic Template By <a href="http://codecerdas.com" target="_blank">codecerdas.com</a> - <a href="https://www.facebook.com/chandika.nurdiansyah.9" target="_blank">Chandika Nurdiansyah</a></li>
 				    <li>3. Videoshoot, Photoshoot And Editing By <a href="https://www.facebook.com/fachreza.i.akbar" target="_blank">Fachreza Iryansyah</a></li>
 				</ul> -->
-			</div>
+      </div>
 
-			<p class="copyright">Copyright &copy; 2017</p>
-		</div>
-	</div>
+      <p class="copyright">Copyright &copy; 2017</p>
+    </div>
+  </div>
 </template>
 
 <script>
